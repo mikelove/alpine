3bce0a604d6a25547c2e19d78aa824f21d9af6b8
Mike Love <michaelisaiahlove@gmail.com>
2013-11-11 13:17:34 -0500

    added GRangesCoverageToNumericCoverage

--- R/core.R
+++ R/core.R
@@ -1,6 +1,6 @@
 # for imported BigWig files, this function adds GRanges with score 0
 # before and after every contiguous region, so that plotting doesn't
-# generate large diagonals
+# generate diagonal lines over large regions
 addGRangesAtZero <- function(x) {
   x <- sort(x)
   idx <- which(start(x)[-1] != end(x)[-length(x)] + 1)
@@ -15,11 +15,11 @@ addGRangesAtZero <- function(x) {
 # at the origin, and removes introns
 exonCoverage <- function(x, gene, addZero=TRUE) {
   x <- sort(x)
-  gene <- sort(gene)
+  gene <- reduce(sort(gene))
   z <- lapply(seq_along(gene), function(i) {
     shiftedGR <- shift(x[x %over% gene[i]], 
                        -start(gene[i]) + 
-                         ifelse(i==1,0,sum(width(gene[seq_len(i-1)]))))
+                         ifelse(i==1,1,sum(width(gene[seq_len(i-1)]))))
     if (addZero) {
       return(c(shiftedGR, GRanges(seqnames(x)[1],
                                   IRanges(sum(width(gene[seq_len(i)])),width=0),
@@ -27,7 +27,21 @@ exonCoverage <- function(x, gene, addZero=TRUE) {
     } else {
       return(shiftedGR) 
     }
-    }
   })
   do.call(c,z)
+}
+
+GRangesCoverageToNumericCoverage <- function(x, gene) {
+  totalWidth <- sum(width(gene))
+  strand <- as.character(strand(gene)[1])
+  d0 <- data.frame(x=start(x), y=mcols(x)$score)
+  lastPos <- d0$x[length(d0$x)]
+  widths <- c(d0$x, lastPos, totalWidth) - c(1, d0$x, lastPos)
+  rlex <- Rle(values=c(0,d0$y,0), lengths=widths)
+  if (strand == "-") {
+    y <- rev(as.numeric(rlex))
+  } else {
+    y <- as.numeric(rlex)
+  }
+  data.frame(x=seq_len(totalWidth-1), y=y)
 }
