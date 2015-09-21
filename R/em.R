9a66cc3a17c95819ce9bdbf5745758f050937a8b
mikelove <mikelove1@gmail.com>
2015-09-21 17:44:26 -0400

    get rid of extra function for single iso

--- R/em.R
+++ R/em.R
@@ -37,7 +37,8 @@ estimateTheta <- function(transcripts, bamfiles, fitpar, genome,
                           subset=FALSE, zerotopos, niter, 
                           lib.sizes=NULL, optim=FALSE, minsize, maxsize) {
   stopifnot(is(transcripts, "GRangesList"))
-  stopifnot(length(transcripts) > 1)
+  stopifnot(length(transcripts) >= 1)
+  singleiso <- length(transcripts) == 1
   stopifnot(all(c("exon_rank","exon_id") %in% names(mcols(transcripts[[1]]))))
   stopifnot(all(names(bamfiles) %in% names(fitpar)))
   stopifnot(all(!is.null(names(bamfiles))))
@@ -45,11 +46,16 @@ estimateTheta <- function(transcripts, bamfiles, fitpar, genome,
   stopifnot(!is.null(fitpar))
   stopifnot(all(names(bamfiles) %in% names(fitpar)))
   stopifnot(!is.null(names(transcripts)))
+  stopifnot(all(file.exists(paste0(bamfiles, ".bai"))))
   if (!is.null(lib.sizes)) stopifnot(all(names(bamfiles) %in% names(lib.sizes)))
-  gene.fco <- findOverlaps(transcripts, ignoreSelf=TRUE)
-  stopifnot(length(gene.fco) > 0)
+  if (!singleiso) {
+    gene.fco <- findOverlaps(transcripts, ignoreSelf=TRUE)
+    stopifnot(length(gene.fco) > 0)
+  }
   w <- sum(width(transcripts))
+  # TODO: come up with a better output here
   if (any(w <= minsize)) return(NULL)
+  
   # TODO: come up with a check on whether models is compatible with fitpar
   
   # this is a list of fragment types for each transcript
@@ -118,7 +124,26 @@ estimateTheta <- function(transcripts, bamfiles, fitpar, genome,
     vlmm.fivep <- fitpar[[bamname]][["vlmm.fivep"]]
     vlmm.threep <- fitpar[[bamname]][["vlmm.threep"]]
     fragtypes <- addVLMMBias(fragtypes, vlmm.fivep, vlmm.threep)
-    
+
+    # specific code for one isoform
+    if (singleiso) {
+      n.obs <- fragtypes$count
+      # this gives list output for one bamfile
+      res.sub <- lapply(model.names, function(modeltype) {
+        log.lambda <- getLogLambda(fragtypes, models, modeltype, fitpar, bamname)
+        N <- if (is.null(lib.sizes)) {
+          mean(n.obs)
+        } else {
+            # TODO: here fix like below
+            lib.sizes[bamname] / (1e9 * (maxsize - minsize))
+          }
+        A <- N * exp(as.numeric(log.lambda))
+        wts <- if (subset) { fragtypes$wts } else { 1 } 
+        sum(n.obs  * wts)/sum(A * wts)
+      })
+      return(res.sub) # return results for this sample
+    }
+
     # make incidence matrix
     # duplicate genomic ID across tx will be a single column 
     mat <- incidenceMat(fragtypes$tx, fragtypes$genomic.id)
@@ -132,7 +157,7 @@ estimateTheta <- function(transcripts, bamfiles, fitpar, genome,
     
     # run EM for different models
     # this gives list output for one bamfile
-    lapply(model.names, function(modeltype) {
+    res.sub <- lapply(model.names, function(modeltype) {
       log.lambda <- getLogLambda(fragtypes.sub, models, modeltype, fitpar, bamname)
       ## pred0 <- as.numeric(exp(log.lambda))
       ## pred <- pred0/mean(pred0)*mean(fragtypes.sub$count)
@@ -149,107 +174,7 @@ estimateTheta <- function(transcripts, bamfiles, fitpar, genome,
       EM.res <- runEM(n.obs, A, wts, niter, optim)
       EM.res
     })
-  })
-  names(res) <- names(bamfiles)
-  res
-}
-estimateThetaSingleIsoform <- function(transcripts, bamfiles, fitpar, genome,
-                                       models, readlength,
-                                       subset=FALSE, zerotopos, niter, 
-                                       lib.sizes=NULL, optim=FALSE, minsize, maxsize) {
-  stopifnot(is(transcripts, "GRangesList"))
-  stopifnot(length(transcripts) == 1)
-  stopifnot(all(c("exon_rank","exon_id") %in% names(mcols(transcripts[[1]]))))
-  stopifnot(all(names(bamfiles) %in% names(fitpar)))
-  stopifnot(all(!is.null(names(bamfiles))))
-  stopifnot(all(file.exists(bamfiles)))
-  stopifnot(!is.null(fitpar))
-  stopifnot(all(names(bamfiles) %in% names(fitpar)))
-  stopifnot(!is.null(names(transcripts)))
-  if (!is.null(lib.sizes)) stopifnot(all(names(bamfiles) %in% names(lib.sizes)))
-  # TODO: come up with a check on whether models is compatible with fitpar
-  
-  # this is a list of fragment types for each transcript
-  st <- system.time({ 
-      out <- buildFragtypesFromExons(transcripts[[1]], genome, readlength, minsize, maxsize)
-    })
-  out$tx <- names(transcripts)
-  fraglist <- list(out)
-
-  # message("building fragment types: ",round(unname(st[3]))," seconds")
-  names(fraglist) <- names(transcripts)
-  res <- lapply(seq_along(bamfiles), function(i) {
-    bamfile <- bamfiles[i]
-    bamname <- names(bamfile)
-    generange <- range(unlist(range(transcripts)))
-    strand(generange) <- "*" # not necessary
-    #message("align reads to txs")
-    suppressWarnings({
-      ga <- readGAlignAlpine(bamfile, generange)
-    })
-    #message("-- ",length(ga)," reads")
-
-    outputZero <- FALSE
-    if (length(ga) == 0) {
-      outputZero <- TRUE
-    } else { 
-      ga <- keepSeqlevels(ga, as.character(seqnames(transcripts[[1]])[1]))
-      fco <- findCompatibleOverlaps(ga, transcripts)
-      #message("-- ",round(length(unique(queryHits(fco)))/length(ga),2)," compatible overlaps")
-      # table(strand(ga)[unique(queryHits(fco))]) # are the read counts even across strand?
-      # boxplot(lapply(reads, function(x) width(x)))
-      # here called "reads" although they are fragments. everything is already called fragments :-/
-      reads <- gaToReadsOnTx(ga, transcripts, fco)
-      fraglist.temp <- matchReadsToFraglist(reads, fraglist)
-      txcounts <- sapply(fraglist.temp, function(x) sum(x$count))
-      if (all(txcounts == 0)) outputZero <- TRUE
-    }
-
-    # report 0 output for all models if all txs have 0 count
-    model.names <- names(models)
-    names(model.names) <- model.names
-    nms.tx <- names(transcripts)
-    if (outputZero) {
-      #message("all transcripts have 0 counts")
-      out <- numeric(length(nms.tx))
-      names(out) <- nms.tx
-      return(lapply(model.names, function(x) out))
-    }
-    
-    if (subset) {
-      #message("subset and weight fragment types")
-      fragtypes <- subsetAndWeightFraglist(fraglist.temp, zerotopos)
-    } else {
-      fragtypes <- do.call(rbind, fraglist.temp)
-    }
-    fragtypes$genomic.id <- paste0(fragtypes$gstart,"-",fragtypes$gread1end,"-",
-                                   fragtypes$gread2start,"-",fragtypes$gend)
-
-    # message("fragment bias")
-    ## -- fragment bias --
-    fraglen.density <- fitpar[[bamname]][["fraglen.density"]]
-    fragtypes$logdfraglen <- log(matchToDensity(fragtypes$fraglen, fraglen.density))
-    # message("priming bias")
-    ## -- random hexamer priming bias with VLMM --
-    vlmm.fivep <- fitpar[[bamname]][["vlmm.fivep"]]
-    vlmm.threep <- fitpar[[bamname]][["vlmm.threep"]]
-    fragtypes <- addVLMMBias(fragtypes, vlmm.fivep, vlmm.threep)
-
-    # from here below, specific code for when we have one isoform
-    n.obs <- fragtypes$count
-    # this gives list output for one bamfile
-    lapply(model.names, function(modeltype) {
-      log.lambda <- getLogLambda(fragtypes, models, modeltype, fitpar, bamname)
-      N <- if (is.null(lib.sizes)) {
-        mean(n.obs)
-      } else {
-        # TODO: see previous function
-        lib.sizes[bamname] / (1e9 * (maxsize - minsize))
-      }
-      A <- N * exp(as.numeric(log.lambda))
-      wts <- if (subset) { fragtypes$wts } else { 1 } 
-      sum(n.obs  * wts)/sum(A * wts)
-    })
+    return(res.sub)
   })
   names(res) <- names(bamfiles)
   res
